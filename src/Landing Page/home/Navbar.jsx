8a403a03397c0c4d7101a35758a13028045f452d
ANURAG SINGH <anuragsingh260805@gmail.com>
2025-11-29 13:50:46 +0530

    Refactor Navbar component with new layout and styles

--- src/Landing Page/home/Navbar.jsx
+++ src/Landing Page/home/Navbar.jsx
@@ -4,7 +4,23 @@ const Navbar = () => {
 
   return (
 
-    <div>Navbar</div>
+    <div className='fixed top-0 flex justify-between items-center mb-8 mx-auto w-full sm:max-w-3xl px-5'>
+      <div className='rounded-md logo'></div>
+      <div className='flex sm:gap-8 gap-5 font-monvex text-[13px] text-gray-600 items-center font-semibold'>
+        <a href="#">
+          About
+        </a>
+        <a href="#">
+          Docs
+        </a>
+        <a href="#">
+          Products
+        </a>
+      </div>
+      <a href="#" className='bg-blue-600 text-white px-3 py-2 rounded-md text-sm font-mono font-semibold sm:ml-4'>
+        Login
+      </a>
+    </div>
     
   )
 }
